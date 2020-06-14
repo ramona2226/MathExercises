c50d893a92c9215bfcffbd1b8730f564cbcc12c7
Ramona2226 <brisc.ramona22@yahoo.com>
2020-06-14 20:04:17 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackIt/App.java
@@ -0,0 +1,20 @@
+package org.fasttrackIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        MethodsClass math = new MetodsClass;
+
+        System.out.println(math.sumOfTwoNum(3, 7));
+
+        System.out.println(math.difence(26, 6));
+
+        System.out.println(math.multiplicstion(2.5));
+
+        System.out.println(math.divizion(36, 6));
+    }
+
+}
